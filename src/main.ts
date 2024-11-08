8a7be93f74d81e0f1a3abd0d4d947539ca57f616
kunman93 <kun.manu93@gmail.com>
2024-11-08 14:48:10 +0100

    feat: create a simple timeline animation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import './style.scss';
 import { gsap } from "gsap";
 
-gsap.to(".box", {
-  x: 200
-});
+// Create a timeline
+/**
+ * repeats the timeline infinite times with a repeat delay of 1s. This will repeat back and forth on each repeat
+ */
+let tl = gsap.timeline({ repeat: -1, repeatDelay: 1, yoyo: true });
+
+// add the tweens to the timeline - Note we're using tl.to not gsap.to
+tl.to(".box", {
+  x: '60vw',
+  rotation: 720,
+  duration: 3, // how long the animation lasts
+  ease: "bounce.out", // Easing is possibly the most important part of motion design.
+  stagger: 0.1, // 0.1 seconds between when each ".box" element starts animating, a negative value would do the same but backwards
+  repeat: 2, // the number of repeats - this will play 3 times
+  yoyo: true // this will repeat back and forth on each repeat. Like a yoyo
+}).to(".green", { rotation: -360, duration: 3, ease: "bounce.out" })
+  .to(".red", { rotation: 360, duration: 2, ease: "back.inOut" });
